c01920e31a8af1c25e79a72788a22be382157be4
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-08-11 12:13:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var isDate = function (input) {
-  //   write your code here
+  // If input is a Date object
+  if (input instanceof Date && !isNaN(input.getTime())) {
+    return true;
+  }
+
+  // If input is not a Date object, try parsing it
+  const parsedDate = new Date(input);
+  return !isNaN(parsedDate.getTime());
 };
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
+     
